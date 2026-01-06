0b1a0c685c1eb74fe68298dc4b37f31c66b21e7d
naveenyeluru <naveen@techiemaya.com>
2026-01-06 13:54:05 +0400

    fix: add deployed frontend origins to Socket.IO CORS

--- core/socket.js
+++ core/socket.js
@@ -13,6 +13,9 @@ async function initSocket(server) {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:3002',
+      'https://lad-frontend-3nddlneyya-uc.a.run.app',
+      'https://lad-frontend-741719885039.us-central1.run.app',
+      'https://lad-frontend-develop-741719885039.us-central1.run.app',
       process.env.FRONTEND_URL
     ].filter(Boolean);
 
